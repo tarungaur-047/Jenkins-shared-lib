32430016fbd65650073a217818f773ffcf3eb79a
Tarun gaur <gaurtarunn2002@gmail.com>
2026-05-05 18:47:24 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+def call(String ImageName, String ImageTag(version), String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
